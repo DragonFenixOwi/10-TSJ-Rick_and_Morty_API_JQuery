cf8e35e91c1b323b3f3b9fc55737205f1dab7ce5
DragonFenixOwi <mecaowi@gmail.com>
2020-06-02 23:06:29 +0200

    Orden de comentario

--- js/Descripcion_Request_profunda.js
+++ js/Descripcion_Request_profunda.js
@@ -5,16 +5,10 @@
 */
 
 /*
-    ---------------------------------------------------------------------------------------------------------------------
+    -------------------------------------------------------------------------------------------------
         Autor: Osvaldo Aquino
         nickname: owi
-        API_link: https://rickandmortyapi.com/api/
-
-        Ha través de esta API, escribir en pantalla del navegador la frase plantilla:
-            - "Hola mi nombre es 'Rick Sanchez', mi especie es 'Human', soy originario de 'Earth (C-137) - location' "
-
-            Que el usuario interesado a través de un 'prompt' pueda pedir diferentes id's.
-    -----------------------------------------------------------------------------------------------------------------------
+    --------------------------------------------------------------------------------------------------
 */
 
 
@@ -27,7 +21,7 @@
 
 
 /*
-    -----------------------------------------------------------------------------------------------------------------
+    ---------------------------------------------
         Desarrollo: 
             - Se realiza un CALLBACK ayudado de una libreria externa (JQuery) para hacer los "request" 
             a una API. 
@@ -59,8 +53,6 @@
                     - pertenece al objeto con "id =1"
                 - Segunda LLamada :  Link_Objeto
                     - pertenece al objeto con "id" introducido por teclado
-
-
     ------------------------------------------------------------------------------------------------------
 */
 
@@ -98,6 +90,7 @@ const OPCIONES = {crossDomain: true};
 
 
 
+
 /*
     ---------------------
         FUNCIONES
@@ -106,11 +99,17 @@ const OPCIONES = {crossDomain: true};
 
 /*
     CALLBACK
-        - Se crea una "constante" denominada "DESCRIPCION_PERSONAJE"
-        - Esa constante es una funcion. Que se convierte en CALLBACK
+        - Se crea una constante denominada "DESCRIPCION_PERSONAJE"
+        - Esa constante es una funcion 
 */
 
+
 // CALLBACK 1 - Se imprime el "nombre y la especie"
+
+/*
+    - Se crea una "constante"  DESCRIPCION_PERSONAJE_CHARACTER
+    - Esta constante es una "función" con un parametro "personaje"
+*/
 const DESCRIPCION_PERSONAJE_CHARACTER = function (personaje)
     {
         //Entre en el "if" solo si el "id=1". Identifica el texto inicial
@@ -119,12 +118,22 @@ const DESCRIPCION_PERSONAJE_CHARACTER = function (personaje)
                     document.write(`# Texto inicial: ${"<br><br>"}`);
                 }
         
-        // Imprime en el navegador "nombre y especie"
+        /* 
+            - Imprime en el navegador "nombre y especie"
+            - Una vez ejecutado ".get" correctamente, con document.write, guardamos toda esa información que se recogio con "get"
+            - No quiero que ejecutes esta función, sin antes haber recogido la información  
+        */
             document.write(` - Hola mi nombre es ${personaje.name}, mi especie es ${personaje.species}, `);
     };
 
 
+
 // CALLBACK 2  - Se imprime el "lugar o locación"
+/*
+    - Se crea una "constante"  DESCRIPCION_PERSONAJE_LOCATION 
+    - Esta constante es una "función" con un parametro "personaje_2"
+*/
+
 const DESCRIPCION_PERSONAJE_LOCATION = function (personaje_2)
     {           
         // Imprime en el navegador "lugar o locación"
@@ -138,19 +147,59 @@ const DESCRIPCION_PERSONAJE_LOCATION = function (personaje_2)
             
     };
 
+
+
+
+
+
+
 /* SEGUNDA LLAMADA - "id " introducido por teclado*/
 
 // Funcion que contiene la dirección de la URL de la API e identificacion "id" introducido por teclado
 function Link_Objeto (id)
     {
-        // Pertence al "Nombre" y la "especie"
-            const URL_1 = `${API_URL}${CHARACTER.replace(":id",id)}`; 
+        /* Pertence al "Nombre" y la "especie" */
+
+        /*
+            - Se crea una constante "URL_1" - tipo string
+            - Se crea la dirección de la URL_1 para llamar a todo "código o objeto" de la API
+            - ${API_URL}https://swapi.dev/api/ ${CHARACTER} (people/:id) completamos el link, 
+                - https://swapi.dev/api/  people/:id
+                - por eso es importante que no haya espacio entre los "${}"
+                - Con ".replace", se reemplaza ":id" escrito en "const PERSONAJES_URL" por el valor introducido por teclado por ejemplo "10" 
+                - "people/10" - es un objeto
+        */
+            const URL_1 = `${API_URL}${CHARACTER.replace(":id",id)}`; //sin espacios los dos singos del dolar.
+
+
+        /*
+            - "$" hace referencia a JQuery 
+            - ".get" nos trae la información 
+            - Descripción de los argumentos dentro del parentesis del "get"
+                -get(¿de donde la  tengo que traer?, Tipo de información(externa), Aqui vamos a insertar el callback 
+            - Mientras "get()" no funcione, no se va a lanzar 
+            - la funcion "DESCRIPCION_PERSONAJE_CHARACTER"  que actua es un CALLBACK
+                */
             $.get(URL_1, OPCIONES, DESCRIPCION_PERSONAJE_CHARACTER);
         
-        // Pertence al "lugar o locación"
+
+        
+        /* Pertence al "lugar o locación" */
+        
+        /*
+            - URL_2 = constante del tipo string que representa el link del API 
+                donde se encuentra el objeto, que contiene 
+                    -API_URL = constante del tipo string declarado en "variables globales" que representa 
+                    el link principal del API donde se encuentra el objeto
+                    - LOCATION = constante del tipo string declarado en "variables globales" que representa 
+                    el link que devuelve el lugar de origen o locación del "id" introducido por teclado
+                    - ".reaplce" remplaza ":id" (variables globales) = id (1 o introducido por teclado)
+                - 
+        */
             const URL_2 = `${API_URL}${LOCATION.replace(":id",id)}`; 
             $.get(URL_2, OPCIONES, DESCRIPCION_PERSONAJE_LOCATION);
     }
+
 
 
 /* LLAMADA INICIAL - "id = 1" */
@@ -158,14 +207,17 @@ function Link_Objeto (id)
 // Funcion que imprime el texto inicial con "id=1"
 function Link_Objeto_constante ()
     {
-        var id = 1;
-        const URL_1 = `${API_URL}${CHARACTER_INICIAL}`; 
-        $.get(URL_1, OPCIONES, DESCRIPCION_PERSONAJE_CHARACTER);
-        const URL = `${API_URL}${LOCATION_INICIAL}`; 
-        $.get(URL, OPCIONES, DESCRIPCION_PERSONAJE_LOCATION);
+        //variable local
+            var id = 1;
+        //Descripción similar en la función "link_objeto" de la "segunda llamada"    
+            const URL_1 = `${API_URL}${CHARACTER_INICIAL}`; 
+            $.get(URL_1, OPCIONES, DESCRIPCION_PERSONAJE_CHARACTER);
+            const URL = `${API_URL}${LOCATION_INICIAL}`; 
+            $.get(URL, OPCIONES, DESCRIPCION_PERSONAJE_LOCATION);
+        
+        // DESCRIPCION_PERSONAJE_LOCATION es una funcion que se convierte en un argumento.
+        
     }
-
-
 
 
 
@@ -193,6 +245,4 @@ function introducir_datos ()
 // Llamada de la función
 introducir_datos();
     
-
-
 
